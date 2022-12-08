f4b3250f06338093de438fbec22194560d3f5022
randymxd06 <randym0624@gmail.com>
2022-12-07 23:44:42 -0400

    Descomente el codigo para crear las ordenes en base a la reorden

--- src/view/pages/payment/SelectedCartPayment.vue
+++ src/view/pages/payment/SelectedCartPayment.vue
@@ -316,23 +316,23 @@ export default {
                     })
                 })
 
-                // let json = {
-                //     order_type_id: this.selectedOrderType,
-                //     payment_method_id: this.selectedPaymentMethod,
-                //     latitude: localStorage.getItem('latitude'),
-                //     longitude: localStorage.getItem('longitude'),
-                //     status: true,
-                //     order_details: this.orderDetails
-                // }
+                let json = {
+                    order_type_id: this.selectedOrderType,
+                    payment_method_id: this.selectedPaymentMethod,
+                    latitude: localStorage.getItem('latitude'),
+                    longitude: localStorage.getItem('longitude'),
+                    status: true,
+                    order_details: this.orderDetails
+                }
 
                 // console.log(JSON.stringify(json, null, 3));
 
-                // await restaurantApi.post('orders', json)
-                // .then(({data}) => {
-                //     console.log(data);
+                await restaurantApi.post('orders', json)
+                .then(({data}) => {
+                    console.log(data);
                     localStorage.setItem('paymentOrders', JSON.stringify(this.selectedCart));
                     this.$router.push({ name: 'selected-order-status' })
-                // })
+                })
                 // .catch(({response}) => {
                 //     console.error(response.data);
                 // })
