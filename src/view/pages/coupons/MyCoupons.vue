5d3d52551a5896c4e2aff8341499b2d389a677c9
randymxd06 <randym0624@gmail.com>
2022-12-13 13:03:52 -0400

    Puse una imagen para cuando no haya cupones

--- src/view/pages/coupons/MyCoupons.vue
+++ src/view/pages/coupons/MyCoupons.vue
@@ -1,69 +1,77 @@
 <template>
-  
-  <main>
 
+  <main>
+    
     <h1 class="mb-10">Mis Cupones</h1>
 
-    <div class="card container">
+    <section v-if="inactiveCoupons.length != 0">
 
-      <section v-for="(item, index) in inactiveCoupons" :key="index">
+      <div class="card container">
 
-        <!-- ITEM -->
-        <div
-          class="d-flex align-items-center rounded p-5 my-5"
-          v-bind:class="`bg-light-${item.color}`"
-        >
+        <section v-for="(item, index) in inactiveCoupons" :key="index">
 
-          <!-- ICON -->
-          <span
-            class="svg-icon mr-5"
-            v-bind:class="`svg-icon-${item.color}`"
+          <!-- ITEM -->
+          <div
+            class="d-flex align-items-center rounded p-5 my-5"
+            v-bind:class="`bg-light-${item.color}`"
           >
-            <!-- <span class="svg-icon svg-icon-lg">
-              <inline-svg :src="item.svg" />
-            </span> -->
-            <img 
-              class="img-fluid" 
-              width="40px" 
-              height="40px" 
-              src="assets/Coupon.png" 
-              alt=""
-            >
-          </span>
 
-          <!-- TITLE -->
-          <div class="d-flex flex-column flex-grow-1 mr-2">
-            <a
-              href="#"
-              class="font-weight-normal text-dark-75 text-hover-primary font-size-lg mb-1"
+            <!-- ICON -->
+            <span
+              class="svg-icon mr-5"
+              v-bind:class="`svg-icon-${item.color}`"
             >
-              {{ item.description }}
-            </a>
-            <span class="text-muted font-size-sm">
-              Cupon valido por {{ item.number_of_days }} días
+              <!-- <span class="svg-icon svg-icon-lg">
+                <inline-svg :src="item.svg" />
+              </span> -->
+              <img 
+                class="img-fluid" 
+                width="40px" 
+                height="40px" 
+                src="assets/Coupon.png" 
+                alt=""
+              >
             </span>
+
+            <!-- TITLE -->
+            <div class="d-flex flex-column flex-grow-1 mr-2">
+              <a
+                href="#"
+                class="font-weight-normal text-dark-75 text-hover-primary font-size-lg mb-1"
+              >
+                {{ item.description }}
+              </a>
+              <span class="text-muted font-size-sm">
+                Cupon valido por {{ item.number_of_days }} días
+              </span>
+            </div>
+
+            <!-- ALT -->
+            <span
+              class="font-weight-bolder py-1 font-size-lg mr-10"
+              v-bind:class="`text-${item.color}`"
+            >
+              -{{ item.percent }}%
+            </span>
+
+            <b-button @click="applyCoupon(item)" class="p-5" variant="primary">
+              <i class="fas fa-check"></i> Aplicar
+            </b-button>
+            
           </div>
 
-          <!-- ALT -->
-          <span
-            class="font-weight-bolder py-1 font-size-lg mr-10"
-            v-bind:class="`text-${item.color}`"
-          >
-            -{{ item.percent }}%
-          </span>
+        </section>
 
-          <b-button @click="applyCoupon(item)" class="p-5" variant="primary">
-            <i class="fas fa-check"></i> Aplicar
-          </b-button>
-          
-        </div>
+      </div>
 
-      </section>
+    </section>
 
-    </div>
+    <section class="m-0 row justify-content-center" v-else>
+      <img class="img-fluid" src="@/view/pages/coupons/assets/images/not_found_image.png" alt="Not Found Image">
+    </section>
 
   </main>
-
+  
 </template>
 
 <script>
